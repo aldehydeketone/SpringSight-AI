c252bc84c922708a93cf942ee72e4c67cdba2d8f
Mihir Singh <mihir304singh@gmail.com>
2026-06-17 16:06:56 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/mihir/springsight/SpringsightAiApplication.java
@@ -0,0 +1,13 @@
+package com.mihir.springsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsightAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsightAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mihir/springsight/SpringsightAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mihir.springsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsightAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
